0d05911d57ee3dfcbe3934ceece6866a6d3c957e
Lucas Vinicius <lucascarvalhoxs@gmail.com>
2025-10-28 05:54:52 -0300

    feat: adiciona atributos e métodos getters / setters

--- Podcast.java
+++ Podcast.java
@@ -1,5 +1,29 @@
 package audioHub;
 
-public class Podcast {
-    
+public class Podcast extends Audio {
+    private String tema;
+    private String nomeDosConvidados;
+    private String descricao;
+    private int curtidas;
+
+    public void setTema(String tema){
+        this.tema = tema;
+    }
+    public String getTema(){
+        return this.tema;
+    }
+
+    public void setNomeDosConvidados(String nomeDosConvidados){
+        this.nomeDosConvidados = nomeDosConvidados;
+    }
+    public String getNomeDosConvidados(){
+        return this.nomeDosConvidados;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
 }
